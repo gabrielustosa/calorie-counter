ec846fbf011edf891624d9e0901553a8996e4bf6
gabriel <lustosaki2@gmail.com>
2022-04-29 16:18:17 -0300

    Finalizando

--- calorie/settings.py
+++ calorie/settings.py
@@ -19,12 +19,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8hhocufy(xh3$x$iepyk7&hpn8pha!ilp!8ua_l3h)=8-lyas%'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.environ.get('DEBUG') == '1' else False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
