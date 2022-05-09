d5d01cdd54a43e351a98fa53c08539c9fc1f2869
Gabriel Lustosa Queiroz <lustosaki2@gmail.com>
2022-05-09 12:11:32 -0300

    Arrumando

--- calorie/apps/user/forms.py
+++ calorie/apps/user/forms.py
@@ -11,8 +11,14 @@ class UserCreateForm(UserCreationForm):
 
     def save(self, commit=True):
         user = super().save(commit=False)
-        user.set_password(self.cleaned_data['password1'])
-        user.email = self.cleaned_data['email']
+        user.set_password(self.cleaned_data["password1"])
+        user.email = self.cleaned_data["username"]
         if commit:
             user.save()
         return user
+
+    def __init__(self, *args, **kwargs):
+        super(UserCreateForm, self).__init__(*args, **kwargs)
+
+        for fieldname in ['username', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
